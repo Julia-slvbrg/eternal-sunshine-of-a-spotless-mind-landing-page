4c4d3b658016665f742283ab5f2c40eacb6becb7
Julia-slvbrg <julia.slvb@gmail.com>
2024-04-08 21:21:36 -0300

    chore: hero implemented

--- gulpfile.js
+++ gulpfile.js
@@ -10,11 +10,14 @@ function styles(){
 
 function images(){
     return gulp.src('./src/images/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'))
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'))
 };
 
-exports.default = gulp.parallel(styles);
+
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', { ignoreInitial: false }, gulp.parallel(styles));
+
 }
