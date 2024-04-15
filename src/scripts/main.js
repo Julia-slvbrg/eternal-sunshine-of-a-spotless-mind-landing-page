53687458e8366ab37b8907bf871b3ccf96058e16
Julia-slvbrg <julia.slvb@gmail.com>
2024-04-14 21:26:55 -0300

    chore: view biographies for actors and characters functionality implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
     const dataBioSelectors = document.querySelectorAll('[data-bio-image]');
     const aboutContainers = document.querySelectorAll('[data-bio-text]');
+    const buttons = document.querySelectorAll('[data-tab-button]');
 
     for(let i = 0; i < dataBioSelectors.length; i++){
         dataBioSelectors[i].addEventListener('click', function(bioSelector){
@@ -17,13 +18,28 @@ document.addEventListener('DOMContentLoaded', function(){
 
                 if(bioSelector.target.dataset.bioImage === aboutContainers[i].dataset.bioText){
                     bioSelectorTabs.classList.add(openSelector);
-                    aboutContainers[i].classList.add('cast__container__about--is-open')
-                    ;
+                    aboutContainers[i].classList.add('cast__container__about--is-open');
                 }
             }
-
         })
     };
+
+    for(let i = 0; i < buttons.length; i++){
+        buttons[i].addEventListener('click', function(button){
+            const targetButton = button.target.dataset.tabButton;
+
+            hideAllTabs();
+            removeActiveBtn();
+
+            const targetTabId = document.querySelector(`[data-tab-id = ${targetButton}]`);
+
+            targetTabId.classList.add('cast__container__about__item--is-active');
+            
+            button.target.classList.add('cast__container__tabs__button--is-active')
+        })
+    }
+
+
 });
 
 function hideBioSelector(){
@@ -34,53 +50,18 @@ function hideBioSelector(){
 
 function hideBioText(){
     const bioTextContainers = document.querySelectorAll('.cast__container__about');
+
     bioTextContainers.forEach(container => container.classList.remove('cast__container__about--is-open'))
 };
 
+function hideAllTabs(){
+    const tabsContent = document.querySelectorAll('.cast__container__about__item');
 
+    tabsContent.forEach(tabContent => tabContent.classList.remove("cast__container__about__item--is-active" ))
+};
 
+function removeActiveBtn(){
+    const buttons = document.querySelectorAll('.cast__container__tabs__button');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    buttons.forEach(button => button.classList.remove('cast__container__tabs__button--is-active'))
+}
